c531d2b96580c82395390bdadd966204e11c52b2
soyombo <soyombo0@naver.com>
2023-01-28 22:08:09 +0300

    Initial Commit

---
+++ miner/minerApp.swift
@@ -0,0 +1,17 @@
+//
+//  minerApp.swift
+//  miner
+//
+//  Created by Soyombo Mantaagiin on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct minerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
